d123ebdea10448fba55309ad310098fc4f464bfa
Uhl Albert <bikrrr@gmail.com>
2024-05-16 10:44:26 -0600

    Initial Commit

---
+++ JournalClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JournalClone
+//
+//  Created by Uhl Albert on 5/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JournalClone/JournalCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalCloneApp.swift
+//  JournalClone
+//
+//  Created by Uhl Albert on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct JournalCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
